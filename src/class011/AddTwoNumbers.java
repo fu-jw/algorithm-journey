996aba94b82d4faa66ca8b23b4773e1a44176004
fu-jw <contact@fu-jw.com>
2024-03-01 07:09:10 +0800

    添加复用老链表的实现方法

--- src/class011/AddTwoNumbers.java
+++ src/class011/AddTwoNumbers.java
@@ -7,54 +7,92 @@ package class011;
 // 测试链接：https://leetcode.cn/problems/add-two-numbers/
 public class AddTwoNumbers {
 
-	// 不要提交这个类
-	public static class ListNode {
-		public int val;
-		public ListNode next;
+    // 不要提交这个类
+    public static class ListNode {
+        public int val;
+        public ListNode next;
 
-		public ListNode(int val) {
-			this.val = val;
-		}
+        ListNode() {
+        }
 
-		public ListNode(int val, ListNode next) {
-			this.val = val;
-			this.next = next;
-		}
-	}
+        public ListNode(int val) {
+            this.val = val;
+        }
 
-	class Solution {
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
-		// 也可以复用老链表
-		// 不过这个实现没有这么做，都是生成的新节点(为了教学好懂)
-		public static ListNode addTwoNumbers(ListNode h1, ListNode h2) {
-			ListNode ans = null, cur = null;
-			int carry = 0;
-			for (int sum, val; // 声明变量
-					h1 != null || h2 != null; // 终止条件
-					h1 = h1 == null ? null : h1.next, // 每一步h1的跳转
-					h2 = h2 == null ? null : h2.next // 每一步h2的跳转
-					) {
+    class Solution {
 
-				sum = (h1 == null ? 0 : h1.val)
-						+ (h2 == null ? 0 : h2.val)
-						+ carry;
+        // 也可以复用老链表
+        // 不过这个实现没有这么做，都是生成的新节点(为了教学好懂)
+        public static ListNode addTwoNumbers(ListNode h1, ListNode h2) {
+            ListNode ans = null, cur = null;
+            int carry = 0;
+            for (int sum, val; // 声明变量
+                 h1 != null || h2 != null; // 终止条件
+                 h1 = h1 == null ? null : h1.next, // 每一步h1的跳转
+                         h2 = h2 == null ? null : h2.next // 每一步h2的跳转
+            ) {
 
-				val = sum % 10;
-				carry = sum / 10;
-				if (ans == null) {
-					ans = new ListNode(val);
-					cur = ans;
-				} else {
-					cur.next = new ListNode(val);
-					cur = cur.next;
-				}
-			}
-			if (carry == 1) {
-				cur.next = new ListNode(1);
-			}
-			return ans;
-		}
+                sum = (h1 == null ? 0 : h1.val)
+                        + (h2 == null ? 0 : h2.val)
+                        + carry;
 
-	}
+                val = sum % 10;
+                carry = sum / 10;
+                if (ans == null) {
+                    ans = new ListNode(val);
+                    cur = ans;
+                } else {
+                    cur.next = new ListNode(val);
+                    cur = cur.next;
+                }
+            }
+            if (carry == 1) {
+                cur.next = new ListNode(1);
+            }
+            return ans;
+        }
+
+        /**
+         * 复用老链表实现
+         */
+        public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+            int carry = 0;
+			int sum =0;
+			ListNode head = new ListNode();
+            ListNode cur = head;
+
+            while (l1 != null && l2 != null) {
+                sum = carry + l1.val + l2.val;
+                carry = sum / 10;
+                cur.next = new ListNode(sum % 10);
+                cur = cur.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            while (l1 != null) {
+                sum = carry + l1.val;
+                carry = sum / 10;
+                cur.next = new ListNode(sum % 10);
+                cur = cur.next;
+                l1 = l1.next;
+            }
+            while (l2 != null) {
+                sum = carry + l2.val;
+                carry = sum / 10;
+                cur.next = new ListNode(sum % 10);
+                cur = cur.next;
+                l2 = l2.next;
+            }
+            if (carry > 0) cur.next = new ListNode(carry);
+            return head.next;
+        }
+
+    }
 
 }
